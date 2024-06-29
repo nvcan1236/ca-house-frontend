c0b6171f961e7a2d9e2f76c3b8464edf73a24eec
NguyenVanCanh <ngcanh1236@gmail.com>
2024-06-29 12:21:01 +0700

    feat: init project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+## Introductioneatures
 
+## Technologies
+
+## Installation
---
+++ TOCAHouse-motel-UI/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h3 className="mt-10 text-3xl font-semibold text-center text-blue-500">Hello world</h3>
+    </>
+  )
+}
+
+export default App
